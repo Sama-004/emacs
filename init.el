6692bbd8be2c8cbab997e2f4a12a2ee367acc6a5
sama004 <70210929+Sama-004@users.noreply.github.com>
2023-02-15 11:33:37 +0530

    updated configs added line mode and removed notification sound

--- init.el
+++ init.el
@@ -6,16 +6,22 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode))))
 
 (defvar my-term-shell "/bin/zsh")
 (defadvice ansi-term (before force-zsh)
   (interactive (list my-term-shell)))
 (ad-activate 'ansi-term)
-
 (defalias 'yes-or-no-p 'y-or-n-p)
-
-
+(require 'org-tempo)
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(global-hl-line-mode t)
+(setq ring-bell-function 'ignore)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 
@@ -31,7 +37,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(wheatgrass))
- '(package-selected-packages '(which-key which\.key use-package)))
+ '(package-selected-packages '(org-bullets which-key which\.key use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
