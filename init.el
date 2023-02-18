b1de32f5b76c445717646aefaac7b5a13747398b
sama004 <70210929+Sama-004@users.noreply.github.com>
2023-02-18 20:40:24 +0530

    removed error (symbol p not defined)

--- init.el
+++ init.el
@@ -28,9 +28,13 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'catppuccin t)
+
+
 (use-package which-key
   :ensure t
-  :config
+  :init
   (which-key-mode))
 
 (package-initialize)
@@ -38,9 +42,7 @@
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'catppuccin t)
+ ;; If there is more than one, they won't work 
  '(package-selected-packages '(org-bullets which-key which\.key use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
